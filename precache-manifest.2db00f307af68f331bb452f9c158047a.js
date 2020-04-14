87e7e95499756bcf4397a0ae058b18ef2607cbb0
Vaibhav Agarwal <vaibhav.agarwal@hilton.com>
2020-04-14 15:32:23 +0100

    Updates

---
+++ precache-manifest.2db00f307af68f331bb452f9c158047a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d78b2c3ae4cb42e89384b1522fd7fec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67ebc2fedd40fab3fc61",
+    "url": "./static/css/main.bd48ed0b.chunk.css"
+  },
+  {
+    "revision": "351b6c4b0b72ad937a8d",
+    "url": "./static/js/2.c685e902.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c685e902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ebc2fedd40fab3fc61",
+    "url": "./static/js/main.8275e30a.chunk.js"
+  },
+  {
+    "revision": "2a43782e58a62e81125a",
+    "url": "./static/js/runtime-main.d3152fb7.js"
+  },
+  {
+    "revision": "b25732c4f9340582b99d946f5cef7774",
+    "url": "./static/media/apple-coffee-computer-desk-356056.b25732c4.jpg"
+  },
+  {
+    "revision": "afd3794ba4efe97a5c3a109be1c097e6",
+    "url": "./static/media/atlas-close-up-dark-dirty-592753.afd3794b.jpg"
+  },
+  {
+    "revision": "bee36bb775304128ae2e20fcb30e4772",
+    "url": "./static/media/background.bee36bb7.jpg"
+  },
+  {
+    "revision": "569c02aa0248f317d5c7fb6eb9d7be3a",
+    "url": "./static/media/blog-blocks-wallpaper-1591056.569c02aa.jpg"
+  },
+  {
+    "revision": "9337b7adf1a772cdd8283d5de5dfa841",
+    "url": "./static/media/landscape-photography-of-rock-formation-near-highway-844167.9337b7ad.jpg"
+  },
+  {
+    "revision": "136ef9451759a1430d7adeaac241c17c",
+    "url": "./static/media/two-brown-pencils-907607.136ef945.jpg"
+  },
+  {
+    "revision": "a554c8cbf0b856a9d5bab89042c6c944",
+    "url": "./static/media/vai.a554c8cb.png"
+  }
+]);
