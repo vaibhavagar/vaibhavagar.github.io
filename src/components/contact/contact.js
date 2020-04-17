bcd2827f0be1d740d9df5f315fcf702f6ceb6e01
Vaibhav Agarwal <vaibhav.agarwal@hilton.com>
2020-04-17 20:14:13 +0100

    update: all files update - final version

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,73 +1,114 @@
 import React from "react";
+// import axios from "axios";
 
 class Contact extends React.Component {
-  state = {
-    userName: "",
-    number: "",
-    details: "",
-    email: "vaibhavagar@gmail.com",
-    submitSuccess: false,
-  };
-
-  handleSubmit = () => {
-    alert("dobne");
-    const templateId = "template_id";
-    this.sendFeedback(templateId, {
-      message_html: this.state.details,
-      from_name: this.state.userName,
-      reply_to: this.state.email,
-    });
-  };
-
-  sendFeedback(templateId, variables) {
-    window.emailjs
-      .send("gmail", templateId, variables)
-      .then((res) => {
-        console.log("Email successfully sent!");
-      })
-      // Handle errors here however you like, or use a React error boundary
-      .catch((err) =>
-        console.error(
-          "Oh well, you failed. Here some thoughts on the error that occured:",
-          err
-        )
-      );
+  constructor() {
+    super();
+    this.state = {
+      userName: "",
+      number: "",
+      details: "",
+      email: "vaibhavagar@gmail.com",
+      submitSuccess: false,
+    };
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  // sendFeedback(templateId, variables) {
+  //   window.emailjs
+  //     .send("gmail", templateId, variables)
+  //     .then((res) => {
+  //       console.log("Email successfully sent!");
+  //     })
+  //     // Handle errors here however you like, or use a React error boundary
+  //     .catch((err) =>
+  //       console.error(
+  //         "Oh well, you failed. Here some thoughts on the error that occured:",
+  //         err
+  //       )
+  //     );
+  // }
+
+  handleSubmit = (e) => {
+    // console.log(this.state);
+    alert(
+      "Backend is not set yet to submit form, please drop an email to vaibhavagar@gmail.com"
+    );
+    // const { email, userName, number, details } = this.state;
+
+    // // create a new XMLHttpRequest
+    // var xhr = new XMLHttpRequest();
+
+    // // get a callback when the server responds
+    // xhr.addEventListener("load", () => {
+    //   // update the response state and the step
+
+    //   this.setState({
+    //     emailStatus: xhr.responseText,
+    //   });
+    // });
+    // // open the request with the verb and the url
+    // xhr.open(
+    //   "GET",
+    //   "../../../index.php?sendto=" +
+    //     email +
+    //     "&name=" +
+    //     userName +
+    //     "number" +
+    //     number +
+    //     "&message=" +
+    //     details
+    // );
+    // // send the request
+    // xhr.send();
+
+    // // reset the fields
+    // this.setState({
+    //   name: "",
+    //   email: "",
+    //   message: "",
+    // });
+
+    e.preventDefault();
+  };
+
   render() {
+    const { userName, number, details } = this.state;
     return (
       <div className="cp-contact">
         <h3>Contact</h3>
         <p>Get in touch if you are looking for a website/app/SEO</p>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
-            <label for="name">Name</label>
+            <label htmlFor="name">Name</label>
             <input
               type="text"
-              name="text"
+              name="name"
               placeholder="name"
               required
               id="name"
+              value={userName}
               onChange={(event) =>
                 this.setState({ userName: event.target.value })
               }
             />
           </div>
           <div>
-            <label for="tel">contact details</label>
+            <label htmlFor="tel">contact details</label>
             <input
               type="tel"
               name="tel"
               id="tel"
               placeholder="contact number"
+              value={number}
               onChange={(event) =>
                 this.setState({ number: event.target.value })
               }
             />
           </div>
           <div>
-            <label for="details">What you are looking for?</label>
-            <input
+            <label htmlFor="details">What you are looking for?</label>
+            {/* <input
               type="text"
               id="details"
               name="details"
@@ -76,15 +117,22 @@ class Contact extends React.Component {
               onChange={(event) =>
                 this.setState({ details: event.target.value })
               }
+            /> */}
+            <textarea
+              className="text-input"
+              id="details"
+              name="details"
+              value={details}
+              onChange={(event) =>
+                this.setState({ details: event.target.value })
+              }
+              placeholder="Enter more details"
+              required
             />
           </div>
           <span class="cp-button">
             <span class="cp-button__ctaBorder"></span>
-            <button
-              className="cp-button__link"
-              type="submit"
-              onClick={this.handleSubmit}
-            >
+            <button className="cp-button__link" type="submit">
               Submit
             </button>
           </span>
